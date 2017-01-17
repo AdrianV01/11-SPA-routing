a72e92995397e3840eed09edfe27604990506d29
Adrian <JS02137@gmail.com>
2017-01-17 15:25:35 -0800

    executed todos with extreme prejudice

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -2,8 +2,11 @@
 
 (function(module) {
   const aboutController = {};
-
-  // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
-
+console.log('About controller');
+  // DONE : Define a function that hides all main section elements, and then reveals just the #about section:
+  aboutController.hideCont = function()
+  {  $('#articles').hide;
+     $('#about').show
+  };
   module.aboutController = aboutController;
 })(window);
--- starter-code/public/scripts/controllers/articleController.js
+++ starter-code/public/scripts/controllers/articleController.js
@@ -1,8 +1,13 @@
 'use strict';
 
 (function(module) {
-  const articleController = {};
-  // TODO: Setup a function that kicks off the fetching and rendering of articles, using the same
+  const articleController = {}
+  articleController.render = function() {
+  Article.fetchAll(articleView.initIndexPage);
+
+
+  };
+  // DONE: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
   // Also be sure to hide all the main section elements, and reveal the #articles section:
 
